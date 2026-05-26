85ee7df7452293c5a0997dac7b5eb9c97a7d3742
jordanlongein-design <jordan.longein@gmail.com>
2026-05-26 20:05:58 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
-const CACHE_NAME='sporttracker-mobile-v6-3-incline-complet-20260526';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE_NAME='sporttracker-mobile-v6-5-red-clean';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
