af9091bdb271b71be0d1980447699e8665e7a266
jordanlongein-design <jordan.longein@gmail.com>
2026-07-02 19:37:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'sporttracker-mobile-v8-1-dc-incline-17-juin-20260617';
+const CACHE_NAME = 'sporttracker-mobile-v8-2-dc-incline-17-juin-20260617';
 
 const ASSETS = [
   './',
   './index.html',
-  './index.html?v=20260701v81',
+  './index.html?v=20260703v82',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
